7bba8e5c12835e98763081a28416ff93f8924911
Hai Hui Wang <haihwang@cn.ibm.com>
2023-09-11 16:13:46 +0800

    update output

--- pre_data.py
+++ pre_data.py
@@ -1,10 +1,13 @@
 # LangChain实现基于本地私有知识库问答的流程：
 # https://blog.csdn.net/qq_36187610/article/details/131900517
 
+# conda activate chatgpt2
+
 # 加载文件 → 读取文件 → 文本分割 → 文本向量化 → 问题向量化 → 在文本向量中匹配与问题最相近的TopK → 匹配出的文本作为上下文与问题一起添加进prompt → 提交给LLM生成回答
 
 # pip install langchain docx2txt sentence_transformers
 # pip install --use-pep517 faiss-gpu
+# pip install faiss-cpu
 
 # 1. 载入本地文档，并切片成若干小片段
 from langchain.document_loaders import Docx2txtLoader
@@ -20,8 +23,8 @@ text_splitter = RecursiveCharacterTextSplitter(chunk_size=256, chunk_overlap=128
 # 切割加载的 document
 split_docs = text_splitter.split_documents(data)
 
-print(len(split_docs))
-print(split_docs[0])
+print('len(split_docs)=>', len(split_docs))
+print('split_docs[0]=>', split_docs[0])
 
 # 2. 基于开源的预训练的Embedding语言模型，对文本进行向量化
 
@@ -34,7 +37,8 @@ import sentence_transformers
 EMBEDDING_MODEL =  "GanymedeNil/text2vec-large-chinese"
 
 embeddings = HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL)
-embeddings.client = sentence_transformers.SentenceTransformer(embeddings.model_name, device='cuda')
+# embeddings.client = sentence_transformers.SentenceTransformer(embeddings.model_name, device='cuda')
+embeddings.client = sentence_transformers.SentenceTransformer(embeddings.model_name, device='cpu')
 
 # 3. 将切分好的文本片段转换为向量，并存入FAISS中
 from langchain.vectorstores import FAISS
@@ -48,10 +52,14 @@ db.save_local("C:/tmp/faiss/") # 指定Faiss的位置
 db = FAISS.load_local("C:/tmp/faiss/",embeddings=embeddings)
 
 # 5. 将问题也转换为文本向量，并在FAISS中查找最为相近的TopK
-question = "新能源行业发展了多久？"
+print('************************************************')
+# question = "新能源行业发展了多久？"
+question = "服务器有几块硬盘？"
+print('question=>', question)
 similarDocs = db.similarity_search(question, include_metadata=True, k=2)
+print('similarDocs=>', similarDocs)
 for x in similarDocs:
-    print(x)
+    print('x=>', x)
 
 # 6. 直接调用LangChain的RetrievalQA，实现基于上下文的问答。省去了写prompt的步骤
 # from langchain.chains import RetrievalQA
